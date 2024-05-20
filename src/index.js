f5d6e82ae57e17e9a83636419f467354b941b900
KristaaP <krista41319@gmail.com>
2024-05-20 19:21:16 -0400

    improved user experience

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function displayPoem(eresponse) {
+function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -10,12 +10,12 @@ function generatePoem(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
-  let apikey = "f96646t50dd29de0oa81b08883b72e29";
+  let apiKey = "f96646t50dd29de0oa81b08883b72e29";
 
   let context =
     "You are  a romatic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HMTL and seperate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning ";
   let prompt = ` User instructions: Generate a poem about ${instructionsInput.value}`;
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
