bad4141daeb61e2b2966e785da09e16964357a9a
KristaaP <krista41319@gmail.com>
2024-05-21 14:07:22 -0400

    fixed errors

--- src/index.js
+++ src/index.js
@@ -13,15 +13,15 @@ function generateRecipe(event) {
   let apiKey = " f96646t50dd29de0oa81b08883b72e29";
 
   let context =
-    "You are a healthy recipe maker and love to write healthy recipes. Your mission is to generate a simple recipe in basic HMTL and seperate each line with a <br/>. Make sure to follow the user instructions. Include a title to the recipe. Sign therecipe with 'SheCodes AI' inside a <strong> element at the end of the recipe and NOT at the beginning ";
-  let prompt = ` User instructions: Generate a healthy recipe about ${instructionsInput.value}`;
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    "You are very passionate master of cooking and love amazing and simple recipes. Your mission is to write delicious, easy to cook and follow recipes. The recipes need to be provided in HTML. The title of the recipe needs to be a <h2> with green color. Include a <h4> with green color with the word Ingredients:. Below Ingredients, include a list using <ul>. Inside the list, add the ingredients to the recipe using measurements and multiple <li> elements. Below this list, include a <h4> with green color with the word Instructions:. Below Instructions, include a list using <ul>. Inside the list, add the instructions to cook the recipe using measurements and multiple <li> elements. Below Sign the poem with 'Enjoy! Happy Cooking or Baking!' inside a <strong> element at the end of the recipe.";
+
+  let prompt = `User Instruction: Generate one dessert recipes based on emotions and moods written in ${instructionsInput}.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement = document.querySelector("#recipe");
   recipeElement.classList.remove("hidden");
   recipeElement.innerHTML = `<div class="generating">⏳ Generating a recipe for ${instructionsInput.value}</div>`;
-
-  axios.get(apiURL).then(displayRecipe);
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
